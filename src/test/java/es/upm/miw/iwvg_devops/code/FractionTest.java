f951d1c56f1ac292b3b0903519ad5986d6f72267
manue <manuem07@ucm.es>
2020-09-23 20:16:38 +0200

    Update Fraction & FractionTest

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -1,7 +1,8 @@
 package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
 
@@ -36,39 +37,39 @@ public class FractionTest {
 
     @Test
     void TestIsProper(){
-        assertEquals(true, new Fraction(2,4).isProper());
+        assertTrue(new Fraction(2,4).isProper());
     }
 
     @Test
     void TestIsImproper(){
-        assertEquals(false, new Fraction(2,4).isImproper());
+        assertFalse(new Fraction(2,4).isImproper());
     }
 
     @Test
     void TestIsEquivalent(){
         Fraction fraction = new Fraction(1,2);
-        assertEquals(true, new Fraction(2,4).isEquivalent(fraction));
+        assertTrue(new Fraction(2,4).isEquivalent(fraction));
     }
 
     @Test
     void TestAdd(){
         Fraction fraction = new Fraction(1,2);
         Fraction fractionResult = new Fraction(8,8);
-        assertEquals(true, new Fraction(2,4).add(fraction).isEquivalent(fractionResult));
+        assertTrue( new Fraction(2,4).add(fraction).isEquivalent(fractionResult));
     }
 
     @Test
     void TestMultiply(){
         Fraction fraction = new Fraction(1,2);
         Fraction fractionResult = new Fraction(2,8);
-        assertEquals(true, new Fraction(2,4).multiply(fraction).isEquivalent(fractionResult));
+        assertTrue( new Fraction(2,4).multiply(fraction).isEquivalent(fractionResult));
     }
 
     @Test
     void TestDivide(){
         Fraction fraction = new Fraction(1,2);
         Fraction fractionResult = new Fraction(4,4);
-        assertEquals(true, new Fraction(2,4).divide(fraction).isEquivalent(fractionResult));
+        assertTrue(new Fraction(2,4).divide(fraction).isEquivalent(fractionResult));
     }
 
     @Test
