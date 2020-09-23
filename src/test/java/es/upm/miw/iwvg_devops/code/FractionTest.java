cfe7b9186e5a7dc29468571d4f7bc0b9fff0f354
manue <manuem07@ucm.es>
2020-09-23 20:13:29 +0200

    Update Fraction & FractionTest

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -35,6 +35,43 @@ public class FractionTest {
     }
 
     @Test
+    void TestIsProper(){
+        assertEquals(true, new Fraction(2,4).isProper());
+    }
+
+    @Test
+    void TestIsImproper(){
+        assertEquals(false, new Fraction(2,4).isImproper());
+    }
+
+    @Test
+    void TestIsEquivalent(){
+        Fraction fraction = new Fraction(1,2);
+        assertEquals(true, new Fraction(2,4).isEquivalent(fraction));
+    }
+
+    @Test
+    void TestAdd(){
+        Fraction fraction = new Fraction(1,2);
+        Fraction fractionResult = new Fraction(8,8);
+        assertEquals(true, new Fraction(2,4).add(fraction).isEquivalent(fractionResult));
+    }
+
+    @Test
+    void TestMultiply(){
+        Fraction fraction = new Fraction(1,2);
+        Fraction fractionResult = new Fraction(2,8);
+        assertEquals(true, new Fraction(2,4).multiply(fraction).isEquivalent(fractionResult));
+    }
+
+    @Test
+    void TestDivide(){
+        Fraction fraction = new Fraction(1,2);
+        Fraction fractionResult = new Fraction(4,4);
+        assertEquals(true, new Fraction(2,4).divide(fraction).isEquivalent(fractionResult));
+    }
+
+    @Test
     void TestToString(){
         assertEquals("Fraction{" +
                 "numerator=" + 2 +
